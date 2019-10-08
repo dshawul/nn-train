4a127ec7c1a9e3c631586db273cac9535ee29579
Daniel Shawul <dshawul@yahoo.com>
2019-10-08 16:48:34 -0600

    Modify value head.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NDIR=$1
 SDIR=$( dirname ${BASH_SOURCE[0]} )
-V=value/Softmax
+V=value/BiasAdd
 P=policy/Reshape
 cp $NDIR/ID-$2-model-0 $NDIR/net-2x32; $SDIR/convert-to-pb.sh ${NDIR}/net-2x32; convert-to-uff $NDIR/net-2x32.pb -O $V -O $P
 cp $NDIR/ID-$2-model-1 $NDIR/net-6x64; $SDIR/convert-to-pb.sh ${NDIR}/net-6x64; convert-to-uff $NDIR/net-6x64.pb -O $V -O $P
