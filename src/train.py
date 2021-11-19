42594170e19325d1dcdad379cf7c858275cd07fe
Daniel Shawul <dshawul@yahoo.com>
2021-11-18 19:35:38 -0700

    Print number of GPUs.

--- src/train.py
+++ src/train.py
@@ -882,6 +882,7 @@ def main(argv):
     #memory growth of gpus
     if args.gpus:
         gpus = tf.config.experimental.list_physical_devices('GPU')
+        print("Num GPUs:", len(gpus))
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
